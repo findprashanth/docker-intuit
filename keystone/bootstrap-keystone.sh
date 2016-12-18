e1211b8a2128303584bec5a6c97f1048a1c105b9
Pramod Venkatesh <vpramo@Pramods-MacBook-Pro.local>
2016-12-18 21:37:08 +0530

    Changed this

--- keystone/bootstrap-keystone.sh
+++ keystone/bootstrap-keystone.sh
@@ -4,4 +4,4 @@ set -x
 SQL_SCRIPT=${SQL_SCRIPT:-/root/keystone.sql}
 mysql -uroot -padmin123 -h mysql <$SQL_SCRIPT
 keystone-manage db_sync
-/usr/bin/keystone-all --config-file=/etc/keystone/keystone.conf --log-file=/var/log/keystone/keystone.log &
+/usr/bin/keystone-all --config-file=/etc/keystone/keystone.conf --log-file=/var/log/keystone/keystone.log 
